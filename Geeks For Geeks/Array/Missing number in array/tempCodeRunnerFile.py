0890aee85aee7f68e86ff2229588fe6a68d5bcc1
Shefa <shefaoudeen123@gmail.com>
2023-12-30 16:08:35 +0530

    completed first challenge in leetCode

---
+++ Geeks For Geeks/Array/Missing number in array/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+N = int(input())
+Array = list(map(int, input().split()))
+missing = None
+
+for i in range (1,N+1):
+  if(i not in Array):
+    missing = i
+    
+print(i)
--- Geeks For Geeks/Array/Winner of an election/Python.py
+++ Geeks For Geeks/Array/Winner of an election/Python.py
@@ -1,30 +1,24 @@
+def winner(arr, n):
+    names = []
+    count = []
+    for i in range(0, n):
+        if (arr[i] not in names):
+            names.append(arr[i])
+    for i in range(0, len(names)-1):
+        count.append(0)
+    for i in range(0, n):
+        for j in range(0, len(names) - 1):
+            if (arr[i] == names[j]):
+                count[j] += 1
+    maxTerm = count[0]
+    maxIndex = 0
+    for i in range(1, len(names)-1):
+        for j in range(i+1, len(names)-1):
+            if (count[i] > maxTerm):
+                maxIndex = int(i)
+    return (f"{names[maxIndex]} {count[maxIndex]}")
+
+
 n = int(input())
-
 array = list(map(str, input().split()))
-
-names = []
-count = []
-
-
-for i in range(0, n):
-    if (array[i] not in names):
-        names.append(array[i])
-
-for i in range(0, len(names)-1):
-    count.append(0)
-
-for i in range(0, n):
-    for j in range(0, len(names) - 1):
-        if (array[i] == names[j]):
-            count[j] += 1
-
-maxTerm = count[0]
-maxIndex = 0
-
-for i in range(1, len(names)-1):
-    for j in range(i+1, len(names)-1):
-
-        if (count[i] > maxTerm):
-            maxIndex = int(i)
-
-print(f"{names[maxIndex]} {count[maxIndex]}")
+print(winner(array, n))
